073e4aeee289b75076cf9aa864b1355cd1cd0e7f
spackperson <61983349+spackperson@users.noreply.github.com>
2020-12-04 15:54:19 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+function rolling_average () {
+    serial.writeValue("roll", roll)
+    total = 0
+    roll_list.unshift(roll)
+    for (let index = 0; index <= roll_list.length - 1; index++) {
+        total = total + roll_list[index]
+    }
+    return_average = total / roll_list.length
+    if (roll_list.length > 10) {
+        trash = roll_list.pop()
+    }
+    serial.writeValue("return_average", return_average)
+}
+let trash = 0
+let roll = 0
+let total = 0
+let return_average = 0
+let roll_list: number[] = []
+roll_list = []
+return_average = 0
+total = 0
+basic.forever(function () {
+    roll = input.rotation(Rotation.Roll)
+    rolling_average()
+})
