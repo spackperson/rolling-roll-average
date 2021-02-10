6fdae2d0826535ec58b24e96d2e92220dac5373d
spackperson <61983349+spackperson@users.noreply.github.com>
2021-02-10 11:19:55 +0100

    Backup

--- main.ts
+++ main.ts
@@ -5,19 +5,19 @@ function rolling_average () {
     for (let index = 0; index <= roll_list.length - 1; index++) {
         total = total + roll_list[index]
     }
-    return_average = total / roll_list.length
+    roll_rollingavg = total / roll_list.length
     if (roll_list.length > 10) {
         trash = roll_list.pop()
     }
-    serial.writeValue("return_average", return_average)
+    serial.writeValue("return_average", roll_rollingavg)
 }
 let trash = 0
 let roll = 0
 let total = 0
-let return_average = 0
+let roll_rollingavg = 0
 let roll_list: number[] = []
 roll_list = []
-return_average = 0
+roll_rollingavg = 0
 total = 0
 basic.forever(function () {
     roll = input.rotation(Rotation.Roll)
